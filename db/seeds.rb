88150a3f424790ca2ec2dd160a354f95f5fc0771
AlexValla59 <alexandrevalla1@gmail.com>
2023-10-20 18:38:05 +0200

    car

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,15 +18,15 @@ Creation.create(nom:'City Break', creation_type: 'Application Web', description:
 
 Creation.create(nom:'Lions Club', creation_type: "Affiches web et print"    , description: 'fpzjefohzlfhzeofnoizejfiozej', photos: 'lions2.jpg', date:'Mars 2023', logiciel:'Adobe ID, AI et PS', poste_occupe: 'Infographiste', entreprise: 'Auto-entrepreneur')
 
-Creation.create(nom:'Gorillas', creation_type: "Newsletters et t-shirts"    ,description: 'zfezefzefzefzefzefzefzefze', photos: 'gorillas2.jpg', date:'2021-2023', logiciel:'InDesign, Illustrator', poste_occupe: 'Warehouse Supervisor', entreprise: 'Gorillas')
+Creation.create(nom:'Gorillas', creation_type: "Newsletters et t-shirts"    ,description: 'zfezefzefzefzefzefzefzefze', photos: 'gorillas2.jpg', date:'2021 à 2023', logiciel:'Adobe ID, AI et PS', poste_occupe: 'Warehouse Supervisor', entreprise: 'Gorillas')
 
-Creation.create(nom:'Plug & Play', creation_type: "Catalogues produits"   ,description: 'efzfedzefezafzefzefze', photos: 'plug2.jpg', date:'2015 et 2019', logiciel:'Photoshop, Indesign', poste_occupe: 'Infographiste', entreprise: 'Auto-entrepreneur')
+Creation.create(nom:'Plug & Play', creation_type: "Catalogues produits"   ,description: 'efzfedzefezafzefzefze', photos: 'plug2.jpg', date:'2015 à 2019', logiciel:'Adobe ID, AI et PS', poste_occupe: 'Infographiste', entreprise: 'Auto-entrepreneur')
 
 Creation.create(nom:'Créations', creation_type: "thdger"   ,description: 'zefzefezfzefzfzefezfzefz', photos: 'alexcrea2.jpg', date:'', logiciel:'', poste_occupe: '', entreprise: '')
 
-Creation.create(nom:'FotoBorne', creation_type: "Support de communication"   ,description: 'efzfzefzefzefzfeezfezf', photos: 'fotoborne2.jpg', date:'2015 à 2020', logiciel:'Photoshop, InDesign', poste_occupe: 'Infographiste', entreprise: 'Fotoborne Animation')
+Creation.create(nom:'FotoBorne', creation_type: "Supports Marketing"   ,description: 'efzfzefzefzefzfeezfezf', photos: 'fotoborne2.jpg', date:'2015 à 2020', logiciel:'Adobe ID, AI et PS', poste_occupe: 'Infographiste', entreprise: 'Fotoborne Animation')
 
-Creation.create(nom:'Mes dessins', creation_type: "sdfzf"   ,description: 'zefzefzefzefzefzefzefez', photos: 'dessins.jpg', date:'', logiciel:'', poste_occupe: '', entreprise: '')
+Creation.create(nom:'Mes dessins', creation_type: "Dessins PAO"   ,description: 'zefzefzefzefzefzefzefez', photos: 'dessins.jpg', date:'2010 à 2023', logiciel:'Adobe IA, ID', poste_occupe: 'Infographiste', entreprise: 'Auto-entrepreneur')
 
 Creation.create(nom:'Autres', creation_type: "sdfz"   ,description: 'efzfzefzefzefzfeezfezf', photos: '', date:'', logiciel:'', poste_occupe: '', entreprise: '')
 
