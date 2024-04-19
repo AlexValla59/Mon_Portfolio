93f2b9153fbe1bbb20311ee6fb6864763d48719a
AlexValla59 <alexandrevalla1@gmail.com>
2024-04-19 15:03:40 +0200

    test

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,22 @@
 class ContactsController < ApplicationController
- 
   def new
     @contact = Contact.new
   end
- 
+
   def create
-    @contact = Contact.new params[:contact]
- 
+    @contact = Contact.new(contact_params)
+
     if @contact.valid?
-      Mailer.contact_form(@contact).deliver 
-      redirect_to new_contact_path, flash: {success: t(:"create.message_has_been_sent")}
+      Mailer.contact_form(@contact).deliver
+      redirect_to new_contact_path, flash: { success: t(:"create.message_has_been_sent") }
     else
       render :new
     end
   end
- 
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:firstname, :lastname, :email, :message, :active)
+  end
 end
