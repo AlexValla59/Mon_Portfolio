9ab2426590a759df768521b3bff2fa3d415ed767
AlexValla59 <alexandrevalla1@gmail.com>
2024-04-19 17:21:54 +0200

    465

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'in-v3.mailjet.com',
-    port: 587,
+    port: 465,
     domain: 'alexandrevalla.com',
     user_name: ENV['MAILJET_USER'],
     password: ENV['MAILJET_PASSWORD'],
